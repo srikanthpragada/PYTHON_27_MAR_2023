1b5f8391f93d802113b94fe61610b4dcee816dd3
srikanthpragada <srikanthpragada@gmail.com>
2023-04-27 19:55:51 +0530

    27-APR

--- demo/oop/person.py
+++ demo/oop/person.py
@@ -6,8 +6,18 @@ class Person:
     def getage(self):
         return self.__age
 
+    @property   # getter method 
+    def category(self):
+        if self.__age < 30:
+            return "Young"
+        else:
+            return "Old"
+
+
 
 p1 = Person("Jeff", 40)
+print(p1.category)  # property
+
 print(p1.__dict__)
 print(p1._Person__age)  # Wrong
 print(p1.getage())  # Right
